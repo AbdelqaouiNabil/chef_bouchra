3257d8935bee95b1996665d86ad79a743c0ae097
AbdelqaouiNabil <61093076+AbdelqaouiNabil@users.noreply.github.com>
2022-03-10 11:15:11 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const burger = document.querySelector(".burger");
+const navlinks = document.querySelector(".navlinks");
+const links = document.querySelectorAll(".navlinks li");
+
+const lines = document.querySelectorAll(".line");
+
+
+burger.addEventListener('click',()=>{
+    navlinks.classList.toggle("open");
+    lines.forEach(line =>{
+        line.classList.toggle("changeColor");
+    });
+    links.forEach(link=>{
+        link.classList.toggle("fade");
+    })
+})
+
+
+
+
+
+
+   
